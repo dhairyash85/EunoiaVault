7849e53105060ac47a2d88240faf970604bc0f9a
ISTE-VESIT <iste.vesit@ves.ac.in>
2025-09-26 09:53:40 +0530

    Updated Team Name

--- eunoiavault/app/layout.tsx
+++ eunoiavault/app/layout.tsx
@@ -114,7 +114,7 @@ export default function RootLayout({
                 <footer className="py-6 md:px-8 md:py-0">
                   <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
                     <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                      Built with care by 4DNC. Always seek professional help in crisis situations.
+                      Built with care by Chaggots. Always seek professional help in crisis situations.
                     </p>
                   </div>
                 </footer>
